cd0592a5b302ed1e81fd0b435b4ff8cb90e58cc0
Michael Flores <deathknigh9911@gmail.com>
2025-07-10 08:47:06 +0200

    PTC test-cbatticon (1251): Automatic Translations July 10, 2025 06:06:36777830

---
+++ languages/cbatticon-pt.l10n.php
@@ -0,0 +1,38 @@
+<?php
+return ['domain'=>NULL,'plural-forms'=>'nplurals=INTEGER; plural=EXPRESSION;','language'=>'','project-id-version'=>'cbatticon 1.6.13','pot-creation-date'=>'2022-04-27 21:27+0200','po-revision-date'=>'YEAR-MO-DA HO:MI+ZONE','messages'=>['Display the version'=>'pt-pt: Display the version','Display debug information'=>'pt-pt: Display debug information','Set update interval (in seconds)'=>'pt-pt: Set update interval (in seconds)','Set icon type (\'standard\', \'notification\' or \'symbolic\')'=>'pt-pt: Set icon type (\'standard\', \'notification\' or \'symbolic\')','Set low battery level (in percent)'=>'pt-pt: Set low battery level (in percent)','Set critical battery level (in percent)'=>'pt-pt: Set critical battery level (in percent)','Command to execute when low battery level is reached'=>'pt-pt: Command to execute when low battery level is reached','Command to execute when critical battery level is reached'=>'pt-pt: Command to execute when critical battery level is reached','Command to execute when left clicking on tray icon'=>'pt-pt: Command to execute when left clicking on tray icon','Hide the notification popups'=>'pt-pt: Hide the notification popups','List available icon types'=>'pt-pt: List available icon types','List available power supplies (battery and AC)'=>'pt-pt: List available power supplies (battery and AC)','[BATTERY ID]'=>'pt-pt: [BATTERY ID]','Cannot parse command line arguments: %s
+'=>'pt-pt: Cannot parse command line arguments: %s
+','cbatticon: a lightweight and fast battery icon that sits in your system tray
+'=>'pt-pt: cbatticon: a lightweight and fast battery icon that sits in your system tray
+','version %s
+'=>'pt-pt: version %s
+','List of available power supplies:
+'=>'pt-pt: List of available power supplies:
+','List of available icon types:
+'=>'pt-pt: List of available icon types:
+','available'=>'pt-pt: available','unavailable'=>'pt-pt: unavailable','Unknown icon type: %s
+'=>'pt-pt: Unknown icon type: %s
+','No icon type found!
+'=>'pt-pt: No icon type found!
+','Invalid update interval! It has been reset to default (%d seconds)
+'=>'pt-pt: Invalid update interval! It has been reset to default (%d seconds)
+','Invalid low level! It has been reset to default (%d percent)
+'=>'pt-pt: Invalid low level! It has been reset to default (%d percent)
+','Invalid critical level! It has been reset to default (%d percent)
+'=>'pt-pt: Invalid critical level! It has been reset to default (%d percent)
+','Critical level is higher than low level! They have been reset to default
+'=>'pt-pt: Critical level is higher than low level! They have been reset to default
+','type: %-*.*s	id: %-*.*s	path: %s
+'=>'pt-pt: type: %%-*.*s	id: %%-*.*s	path: %s
+','Battery'=>'pt-pt: Battery','AC'=>'pt-pt: AC','Cannot open sysfs directory: %s (%s)
+'=>'pt-pt: Cannot open sysfs directory: %s (%s)
+','No battery with suffix %s found!
+'=>'pt-pt: No battery with suffix %s found!
+','No battery nor AC power supply found!
+'=>'pt-pt: No battery nor AC power supply found!
+','AC only, no battery!'=>'pt-pt: AC only, no battery!','Spawning low battery level command in 5 seconds: %s'=>'pt-pt: Spawning low battery level command in 5 seconds: %s','Skipping low battery level command, no longer discharging'=>'pt-pt: Skipping low battery level command, no longer discharging','Cannot spawn low battery level command: %s
+'=>'pt-pt: Cannot spawn low battery level command: %s
+','Cannot spawn low battery level command!'=>'pt-pt: Cannot spawn low battery level command!','Spawning critical battery level command in 30 seconds: %s'=>'pt-pt: Spawning critical battery level command in 30 seconds: %s','Skipping critical battery level command, no longer discharging'=>'pt-pt: Skipping critical battery level command, no longer discharging','Cannot spawn critical battery level command: %s
+'=>'pt-pt: Cannot spawn critical battery level command: %s
+','Cannot spawn critical battery level command!'=>'pt-pt: Cannot spawn critical battery level command!','Cannot spawn left click command: %s
+'=>'pt-pt: Cannot spawn left click command: %s
+','Cannot spawn left click command!'=>'pt-pt: Cannot spawn left click command!','Battery is missing!'=>'pt-pt: Battery is missing!','Battery status is unknown!'=>'pt-pt: Battery status is unknown!','Battery is charged!'=>'pt-pt: Battery is charged!','Battery is discharging (%i%% remaining)'=>'pt-pt: Battery is discharging (%i%% remaining)','Battery is not charging (%i%% remaining)'=>'pt-pt: Battery is not charging (%i%% remaining)','Battery level is low! (%i%% remaining)'=>'pt-pt: Battery level is low! (%i%% remaining)','Battery level is critical! (%i%% remaining)'=>'pt-pt: Battery level is critical! (%i%% remaining)','Battery is charging (%i%%)'=>'pt-pt: Battery is charging (%i%%)','%d minute'=>'pt-pt: %d minute' . "\0" . 'pt-pt: %d minutes','%d hour, %s remaining'=>'pt-pt: %d hour, %s remaining' . "\0" . 'pt-pt: %d hours, %s remaining','%d minute remaining'=>'pt-pt: %d minute remaining' . "\0" . 'pt-pt: %d minutes remaining']];
